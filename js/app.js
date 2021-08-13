5d88e3d58fa099d0f0d2001acfa88de8b88eca38
Skye M <skyemc608@gmail.com>
2021-08-12 23:25:58 -0500

    finished creating the app script

--- js/app.js
+++ js/app.js
@@ -1,36 +1,41 @@
-'use strict';
-
+"use strict";
 // Cart constructor.
-const Cart = function(items) { 
+const Cart = function (items) {
   // this.items is an array of CartItem instances.
   this.items = items;
 };
 
-Cart.prototype.addItem = function(product, quantity) {
+Cart.prototype.addItem = function (product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
-  let newItem = new CartItem // here is where we create a new CartItem
-  (product, quantity); // product and quantity are our properties
-  this.items.push(newItem); // here we are pushing the data to our items array
+  let newItem = new CartItem(product, quantity); // Adding a new cart item
+  this.items.push(newItem); // here we are pushing the new item to the array
+  console.log(this.items); // debugger successful
 };
- 
+
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
-  
+  localStorage.setItem("Cart", JSON.stringify(this.items)); // stringify converts the array into a string so it is in the correct format
+  // setItem saves it to localStorage 
 };
 
-Cart.prototype.removeItem = function(item) {
+Cart.prototype.removeItem = function (item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
-
+  for (let i = 0; i < this.items.length; i++) {
+    if (this.items[i].product === item) {
+      this.items.splice(i, 1); // this removes an item from the array
+    }
+  }
+  this.saveToLocalStorage();
 };
 
-const CartItem = function(product, quantity) {
+const CartItem = function (product, quantity) {
   this.product = product;
   this.quantity = quantity;
 };
 
-// Product contructor.
-const Product = function(filePath, name) {
+// Product constructor.
+const Product = function (filePath, name) {
   this.filePath = filePath;
   this.name = name;
   Product.allProducts.push(this);
@@ -38,25 +43,25 @@ const Product = function(filePath, name) {
 Product.allProducts = [];
 
 function generateCatalog() {
-  new Product('assets/bag.jpg', 'Bag');
-  new Product('assets/banana.jpg', 'Banana');
-  new Product('assets/bathroom.jpg', 'Bathroom');
-  new Product('assets/boots.jpg', 'Boots');
-  new Product('assets/breakfast.jpg', 'Breakfast');
-  new Product('assets/bubblegum.jpg', 'Bubblegum');
-  new Product('assets/chair.jpg', 'Chair');
-  new Product('assets/cthulhu.jpg', 'Cthulhu');
-  new Product('assets/dog-duck.jpg', 'Dog-Duck');
-  new Product('assets/dragon.jpg', 'Dragon');
-  new Product('assets/pen.jpg', 'Pen');
-  new Product('assets/pet-sweep.jpg', 'Pet Sweep');
-  new Product('assets/scissors.jpg', 'Scissors');
-  new Product('assets/shark.jpg', 'Shark');
-  new Product('assets/sweep.png', 'Sweep');
-  new Product('assets/tauntaun.jpg', 'Taun-Taun');
-  new Product('assets/unicorn.jpg', 'Unicorn');
-  new Product('assets/water-can.jpg', 'Water Can');
-  new Product('assets/wine-glass.jpg', 'Wine Glass');
+  new Product("assets/bag.jpg", "Bag");
+  new Product("assets/banana.jpg", "Banana");
+  new Product("assets/bathroom.jpg", "Bathroom");
+  new Product("assets/boots.jpg", "Boots");
+  new Product("assets/breakfast.jpg", "Breakfast");
+  new Product("assets/bubblegum.jpg", "Bubblegum");
+  new Product("assets/chair.jpg", "Chair");
+  new Product("assets/cthulhu.jpg", "Cthulhu");
+  new Product("assets/dog-duck.jpg", "Dog-Duck");
+  new Product("assets/dragon.jpg", "Dragon");
+  new Product("assets/pen.jpg", "Pen");
+  new Product("assets/pet-sweep.jpg", "Pet Sweep");
+  new Product("assets/scissors.jpg", "Scissors");
+  new Product("assets/shark.jpg", "Shark");
+  new Product("assets/sweep.png", "Sweep");
+  new Product("assets/tauntaun.jpg", "Taun-Taun");
+  new Product("assets/unicorn.jpg", "Unicorn");
+  new Product("assets/water-can.jpg", "Water Can");
+  new Product("assets/wine-glass.jpg", "Wine Glass");
 }
 
 // Initialize the app by creating the big list of products with images and names
